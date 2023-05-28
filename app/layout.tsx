3a6553f0aa4ad31f0b9dc936698a916e184dfc4a
hyoloui <tmdgy9272@gmail.com>
2023-05-29 02:11:44 +0900

    feat: Modal UI
    - Modal 컴포넌트 생성
    - 공용 Button , input 생성
    - 모달 임시 axios 통신 + toast 적용
    - zustand를 사용해 회원가입 토글 적용
    - ADD: axios, react-hook-form, react-hot-toast, zustand
    
    #2 @hyoloui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import RegisterModal from "./components/modal/RegisterModal";
+import ToasterProvider from "./components/providers/ToasterProvider";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -17,6 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
